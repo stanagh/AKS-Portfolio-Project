b0898d5e9bdd78a88be410568e400bf33f1416d8
Stan <oderaghara5@icloud>
2025-09-21 19:25:55 +0100

    Fixed typo on aks output.tf

--- terraform/modules/aks/output.tf
+++ terraform/modules/aks/output.tf
@@ -5,7 +5,7 @@ output "aks_cluster_name" {
 
 output "kubelet_identity_object_id" {
   description = "The object ID of the AKS cluster's kubelet identity"
-  value       = azurerm_kubernetes_cluster.aks_cluster.kubelet_identity[0].object_ids
+  value       = azurerm_kubernetes_cluster.aks_cluster.kubelet_identity[0].object_id
 }
 
 output "aks_cluster_id" {

df1c293f4a20a6e0a8e8463c4ce7b9610492e608
Stan <oderaghara5@icloud>
2025-10-14 18:37:21 +0100

    Downgraded provider version to fix pipeline issue

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,13 +2,16 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.44.0"
+      version = "3.66.0"
     }
   }
+  required_version = ">= 1.0.0"
 }
 
+
 provider "azurerm" {
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
+  # subscription_id = var.subscription_id
+  # tenant_id       = var.tenant_id
   features {}
+  skip_provider_registration = true
 }

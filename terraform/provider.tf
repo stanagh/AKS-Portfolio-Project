2ea59d7f2ca89c5ccceff20c17378071377d6688
Stan <oderaghara5@icloud>
2025-10-15 19:57:54 +0100

    .

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,10 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.66.0"
+      # version = "3.66.0"
+      version = "4.48.0"
     }
   }
-  required_version = ">= 1.0.0"
+  # required_version = ">= 1.0.0"
 }
 
 
@@ -15,5 +16,4 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false  # Set to true to prevent deletion of resource groups that contain resources
     }
   }
-  skip_provider_registration = true
 }

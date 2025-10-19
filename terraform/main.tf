f341d2b343681d479806ba80a06493057c311c05
Stan <oderaghara5@icloud>
2025-10-19 17:30:16 +0100

    .

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,92 +16,86 @@ resource "azurerm_resource_group" "rg" {
   tags     = local.tags
 }
 
-module "storage" {
-  source                   = "./modules/storage"
-  storage_account_name     = "${local.location_short}storage${local.environment}${random_integer.suffix.result}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = local.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  tags                     = local.tags
-  min_tls_version          = "TLS1_2"
-}
+# module "storage" {
+#   source                   = "./modules/storage"
+#   storage_account_name     = "${local.location_short}storage${local.environment}${random_integer.suffix.result}"
+#   resource_group_name      = azurerm_resource_group.rg.name
+#   location                 = local.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+#   tags                     = local.tags
+#   min_tls_version          = "TLS1_2"
+# }
 
-module "keyvault" {
-  source                      = "./modules/keyvault"
-  key_vault_name              = "${local.location_short}kv${local.environment}-${random_integer.suffix.result}"
-  location                    = local.location
-  resource_group_name         = azurerm_resource_group.rg.name
-  enabled_for_disk_encryption = true
-  tags                        = local.tags
-  soft_delete_retention_days  = 7
-  sku_name                    = "standard"
-  key_vault_secret_grafana    = "grafana-admin-password"
-  grafana_admin_password      = random_password.grafana_admin.result
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-}
+# module "keyvault" {
+#   source                      = "./modules/keyvault"
+#   key_vault_name              = "${local.location_short}kv${local.environment}-${random_integer.suffix.result}"
+#   location                    = local.location
+#   resource_group_name         = azurerm_resource_group.rg.name
+#   enabled_for_disk_encryption = true
+#   tags                        = local.tags
+#   soft_delete_retention_days  = 7
+#   sku_name                    = "standard"
+#   key_vault_secret_grafana    = "grafana-admin-password"
+#   grafana_admin_password      = random_password.grafana_admin.result
+#   tenant_id                   = data.azurerm_client_config.current.tenant_id
+# }
 
-resource "azurerm_role_assignment" "keyvault_access" {
-  principal_id         = data.azurerm_client_config.current.object_id
-  role_definition_name = "Key Vault Administrator"
-  scope                = module.keyvault.key_vault_id
-  depends_on           = [module.keyvault]
-}
 
-module "acr" {
-  source              = "./modules/acr"
-  acr_name            = "${local.location_short}acr${local.environment}${random_integer.suffix.result}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = local.location
-  sku                 = "Standard"
-  admin_enabled       = false
-  tags                = local.tags
-}
+# module "acr" {
+#   source              = "./modules/acr"
+#   acr_name            = "${local.location_short}acr${local.environment}${random_integer.suffix.result}"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = local.location
+#   sku                 = "Standard"
+#   admin_enabled       = false
+#   tags                = local.tags
+# }
 
-resource "azurerm_role_assignment" "acr_pull" {
-  principal_id         = module.aks.kubelet_identity_object_id
-  role_definition_name = "AcrPull"
-  scope                = module.acr.acr_id
-  depends_on           = [module.aks, module.acr]
-}
+# resource "azurerm_role_assignment" "acr_pull" {
+#   principal_id         = module.aks.kubelet_identity_object_id
+#   role_definition_name = "AcrPull"
+#   scope                = module.acr.acr_id
+#   depends_on           = [module.aks, module.acr]
+# }
 
-module "aks" {
-  source                            = "./modules/aks"
-  aks_cluster_name                  = "${local.location_short}-aks-${local.environment}-${random_integer.suffix.result}"
-  resource_group_name               = azurerm_resource_group.rg.name
-  location                          = local.location
-  dns_prefix                        = "${local.prefix}-aks-${local.environment}"
-  node_count                        = 2
-  node_vm_size                      = "Standard_B2s"
-  identity_type                     = "SystemAssigned"
-  tags                              = local.tags
-  role_based_access_control_enabled = true
-}
+# module "aks" {
+#   source                            = "./modules/aks"
+#   aks_cluster_name                  = "${local.location_short}-aks-${local.environment}-${random_integer.suffix.result}"
+#   resource_group_name               = azurerm_resource_group.rg.name
+#   location                          = local.location
+#   dns_prefix                        = "${local.prefix}-aks-${local.environment}"
+#   node_count                        = 2
+#   node_vm_size                      = "Standard_B2s"
+#   identity_type                     = "SystemAssigned"
+#   tags                              = local.tags
+#   role_based_access_control_enabled = true
+# }
 
-module "dns" {
-  source              = "./modules/dns"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = local.location
-  dns_name            = "stanley-portfolio.com"
-  a_record_name       = "dev"
-  public_ip_name      = "${local.environment}-pip-${local.prefix}-${random_integer.suffix.result}"
-  public_ip_sku       = "Standard"
-  ttl                 = 300
-  tags                = local.tags
-}
+# module "dns" {
+#   source              = "./modules/dns"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = local.location
+#   dns_name            = "stanley-portfolio.com"
+#   a_record_name       = "dev"
+#   public_ip_name      = "${local.environment}-pip-${local.prefix}-${random_integer.suffix.result}"
+#   public_ip_sku       = "Standard"
+#   ttl                 = 300
+#   tags                = local.tags
+# }
 
-module "aks_monitoring" {
-  source                  = "./modules/k8s_monitoring"
-  prometheus_release_name = "prometheus"
-  prometheus_repository   = "https://prometheus-community.github.io/helm-charts"
-  prometheus_chart        = "kube-prometheus-stack"
-  create_namespace        = true
-  grafana_reportory       = "https://grafana.github.io/helm-charts"
-  grafana_chart           = "grafana"
-  grafana_release_name    = "grafana"
-  namespace               = "monitoring"
-  grafana_admin_password  = random_password.grafana_admin.result
-  depends_on              = [module.aks, module.keyvault]
-}
+# module "aks_monitoring" {
+#   source                  = "./modules/k8s_monitoring"
+#   prometheus_release_name = "prometheus"
+#   prometheus_repository   = "https://prometheus-community.github.io/helm-charts"
+#   prometheus_chart        = "kube-prometheus-stack"
+#   create_namespace        = true
+#   grafana_reportory       = "https://grafana.github.io/helm-charts"
+#   grafana_chart           = "grafana"
+#   grafana_release_name    = "grafana"
+#   namespace               = "monitoring"
+#   grafana_admin_password  = random_password.grafana_admin.result
+#   depends_on              = [module.aks, module.keyvault]
+# }
 
 

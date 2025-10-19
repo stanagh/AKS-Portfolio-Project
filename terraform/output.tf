860dc422f95172ead77621b9f3fa9224955dfe96
Stan <oderaghara5@icloud>
2025-10-19 21:55:41 +0100

    .

--- terraform/output.tf
+++ terraform/output.tf
@@ -3,12 +3,12 @@ output "resource_group_name" {
   value       = azurerm_resource_group.rg.name
 }
 
-# output "acr_name" {
-#   description = "The name of the Azure Container Registry"
-#   value       = module.acr.acr_name
-# }
+output "acr_name" {
+  description = "The name of the Azure Container Registry"
+  value       = module.acr.acr_name
+}
 
-# output "aks_cluster_name" {
-#   description = "The name of the AKS cluster"
-#   value       = module.aks.aks_cluster_name
-# }
+output "aks_cluster_name" {
+  description = "The name of the AKS cluster"
+  value       = module.aks.aks_cluster_name
+}
